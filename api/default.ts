01b35ddbdff6288397ab6b86114376e2ec3812ab
joe <doncrafttv@gmail.com>
2022-10-20 17:22:06 +0300

    husky test

--- api/default.ts
+++ api/default.ts
@@ -1,5 +1,5 @@
-const msg = 'Hello world!';
+const msg = "Hello world!";
 
 export default function handler(req: any, res: any) {
-    res.status(200).json({ msg: msg });
+  res.status(200).json({ msg: msg });
 }
